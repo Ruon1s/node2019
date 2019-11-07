e0dca29a425c9070301d77a94b67350c2e125ba6
Mikael Ruonala <Mikael.Ruonala@Metropolia.fi>
2019-11-07 13:22:49 +0200

    express app, get / and get /demo

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
 'use strict';
 
 console.log('my app');
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+app.get('/demo', (req, res) => {
+    console.log('request', req);
+    res.send('demo');
+})
+
+app.listen(3000);
