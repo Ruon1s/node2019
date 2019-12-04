51727486a0d4452f100ad469f3d1bc84988daefc
Mikael Ruonala <Mikael.Ruonala@Metropolia.fi>
2019-12-04 10:57:58 +0200

    users

--- index.js
+++ index.js
@@ -2,7 +2,15 @@
 
 const express = require('express');
 const animal = require('./model/animal');
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+const bcrypt = require('bcryptjs');
 const app = express();
+
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(express.urlencoded({extended: true}));
+app.use(require('express-session')({secret: 'keyboard cat', resave: true, saveUninitialized: true }));
 
 if(process.env.SERVER === 'dev_localhost') {
   require('./secure/localhost')(app);
@@ -10,12 +18,48 @@ if(process.env.SERVER === 'dev_localhost') {
   require('./secure/server')(app);
   app.listen(3000, () => {
     console.log('server app start?');
-  })
+  });
 }
+
+passport.use(new LocalStrategy(
+    (username, password, done) => {
+      console.log('login', username);
+  if(username !== 'test' || !bcrypt.compareSync(password, '$2a$12$Cdo1wfDRQZM/tlbGLR3AtuplXIFkzn5FdRgCl5LFpks1VXJ02Dyvy')) {
+    console.log('login', 'wrong username or password');
+    return done(null, false);
+  }
+    return done(null, {username: username});
+  }));
+
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser((username, done) => {
+  /* User.findById(id, function (err, user) {
+  done(err, user); */
+  done(null, {username: username});
+});
+
+
+app.post('/login',
+    passport.authenticate('local', {failureRedirect: '/login'},
+    (req, res) => {
+      res.redirect('/');
+    }));
+
+app.post('/register', (req, res) => {
+  const salt = bcrypt.genSaltSync(12);
+  const hash = bcrypt.hashSync(req.body.password, salt);
+  //insert into user (name, email, password) values (?, ?, ?), [ req.body.name, req.body.email, hash]
+  console.log('NEVER DO THAT', hash);
+  res.send('account successfully created');
+});
 
 const bodyParser = require('body-parser');
 
 app.use(express.static('public'));
+
 
 app.get('/animals', async (req, res) => {
   try {
@@ -57,7 +101,3 @@ app.get('/demo', (req, res) => {
   console.log('request', req);
   res.send('demo');
 });
-
-/*app.listen(3000, () => {
-  console.log('server app start?');
-}); */
